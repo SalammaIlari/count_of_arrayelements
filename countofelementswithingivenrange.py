98788cc9e901e494075db22568d24b8b37946842
SalammaIlari <51205520+SalammaIlari@users.noreply.github.com>
2021-03-20 23:51:21 +0530

    Add files via upload
    count of array elements with in the given range

---
+++ countofelementswithingivenrange.py
@@ -0,0 +1,18 @@
+# function to count elements within given range 
+def countInRange(arr, n, x, y): 
+	count = 0; 
+	for i in range(n): 
+		if (arr[i] >= x and arr[i] <= y): 
+			count += 1
+	return count 
+arr = [1,3,5,6,8,9,0] 
+n = len(arr) 
+
+# Answer queries 
+i = 1
+j = 5
+print(countInRange(arr, n, i, j))
+i = 6
+j = 9
+print(countInRange(arr, n, i, j)) 
+
